427bbdcba9b995547f221ed526d6cade480a83c9
Ashkan Mohammadi <mohammadiashkan1384@gmail.com>
2021-07-12 15:27:58 +0430

    rename function parameters
    details:
    - correct some grammatical erros
    - use absolute path to save screenshots

--- modules/kid_keeper.py
+++ modules/kid_keeper.py
@@ -1,15 +1,19 @@
-from pyautogui import screenshot
+from os import path
 import time
+import pathlib
 from colorama import Fore as c
+from pyautogui import screenshot
+from .settings import BASE_DIR
 
-def take_screenshot(number_of_take_screenshot, waiting_time):
+
+def take_screenshot(screenshots: int, delay: float):
     try:
-        number_of_take_screenshot = number_of_take_screenshot + 1
-        print(c.RED + "\n [!] Start Keeping . . .")
-        for i in range(1, number_of_take_screenshot):
+        screenshots = screenshots + 1
+        print(c.RED + "\n [!] Keeping Started . . .")
+        for i in range(1, screenshots):
             screenshot_image = screenshot()
-            screenshot_image.save(r"./screenshot_images/screenshot" + str(i) + ".png")
-            print(c.GREEN + "Toke Screenshot", str(i) + " Successfuly!")
-            time.sleep(waiting_time)
+            screenshot_image.save((BASE_DIR / (r"./screenshot_images/screenshot" + str(i) + ".png")).resolve())
+            print(c.GREEN + "Took Screenshot", str(i) + " Successfuly!")
+            time.sleep(delay)
     except Exception as err:
         print(err)
